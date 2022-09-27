29a91d38abfcfed9c79fb51e1fd1b74b634ce853
LeeHyunji <hyunji015@gmail.com>
2022-09-27 09:47:03 +0900

    쪽지시험 - 02 - Django 프로젝트에 Model을 생성하고 접속 로그 기록해보기
    파이참의 프로젝트 자동 생성 및 프로젝트 실행 기능 없이 명령어로만 문제 풀어보기
    문제 >>
    	1. models.py에 AccessLog 테이블을 생성해주세요
    	* 필드
        		* created_at : 데이터가 생성된 시간을 알 수 있도록 datetimefield 사용
        		* location : 사용자가 어떤 template에 접근했는지 알 수 있도록 charfield 사용
    	2. 생성된 테이블을 makemigrations / migrate 명령어를 통해 db에 반영해주세요
    	3. 생성한 테이블을 admin.py에 등록해주세요
    	4. 사용자가 introduce 페이지에 접속했을 때, 접속 로그를 남겨주세요
    		* admin 페이지에서 로그를 봤을 때 언제 어떤 페이지에 접속했는지 알 수 있어야 합니다.
    		* 로그를 저장할 때는 어떤 탬플릿을 조회했는지도 확인할 수 있도록 해주세요
    	5. admin 페이지에 접속해서 접속 기록을 확인해주세요

--- introduce/models.py
+++ introduce/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class AccessLog(models.Model):
+    class Meta:
+        db_table = "access_log"
+
+    location = models.CharField(max_length=256, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
--- introduce/views.py
+++ introduce/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import AccessLog
 
 # Create your views here.
 def introduce(request):
+    new_access = AccessLog()
+    new_access.location = request.path+'introduce.html'
+    new_access.save()
+
     return render(request, 'introduce.html')
